6fd3240155169b41326e69b5f6dfb11ab9217b26
J0SHUALU <j.moses@alustudent.com>
2024-11-09 17:45:11 +0200

    5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""a class BaseGeometry (based on 5-base_geometry.py)."""
+"""an empty class BaseGeometry."""
 
 
 class BaseGeometry:
     """Represent base geometry."""
-
-    def area(self):
-        """Not implemented."""
-        raise Exception("area() is not implemented")
+    pass
