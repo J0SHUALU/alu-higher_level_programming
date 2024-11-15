f09747410f489d5a5fefa5b35f79202080763891
J0SHUALU <j.moses@alustudent.com>
2024-11-15 17:13:15 +0200

    changes

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table (id, name, score) VALUES(1, 'Boris', 10);
-INSERT INTO second_table (id, name, score) VALUES(2, 'Messi', 3);
-INSERT INTO second_table (id, name, score) VALUES(3, 'Yamal', 14);
-INSERT INTO second_table (id, name, score) VALUES(4, 'Kate', 8);
+INSERT INTO second_table (id, name, score) VALUES(1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES(2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES(3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES(4, 'George', 8);
