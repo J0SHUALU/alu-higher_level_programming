bb750740c908c01accd4f351d94c32dae3188fad
Joshualu <j.moses@alustudent.com>
2025-02-05 18:29:47 +0200

    6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-for (const str of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-    console.log(str);
-  }
-  
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i of arr) {
+  console.log(i);
+}
+
