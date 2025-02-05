930d855970d60b379cdaf94d58b1d6bc056a67eb
Joshualu <j.moses@alustudent.com>
2025-02-05 18:53:01 +0200

    999

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+add(process.argv[2], process.argv[3]);
+
 function add (a, b) {
-    console.log(a + b);
-  }
-  add(parseInt(process.argv[2]), parseInt(process.argv[3]));
-  
+  console.log(+a + +b);
+}
+
