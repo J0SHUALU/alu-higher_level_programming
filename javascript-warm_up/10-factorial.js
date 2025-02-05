e8b67d2a059b27002d37f0cb13d49fdbb0372d95
Joshualu <j.moses@alustudent.com>
2025-02-05 19:05:43 +0200

    TEN

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,11 +4,11 @@ const arg = +process.argv[2];
 if (isNaN(arg)) {
   console.log(1);
 } else {
-    console.log(fact(arg));
+  console.log(fact(arg));
 }
 
 function fact (n) {
-    if (n === 1) return n;
+  if (n === 1) return n;
 
-    return n * fact(n - 1);
+  return n * fact(n - 1);
 }
