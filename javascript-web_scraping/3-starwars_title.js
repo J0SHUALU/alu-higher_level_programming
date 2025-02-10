fb8a07dc91986177e83bc169e83f6a66e69c7462
Joshualu <j.moses@alustudent.com>
2025-02-10 20:35:09 +0200

    chnages

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (err, res, body) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`code: ${res.statusCode}`);
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
-});
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
